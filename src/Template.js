a1b87b9aad3a0029ed236d6337f74a12e3ba5fe0
Ryan Willett <rwillett@ucsd.edu>
2018-03-01 11:52:53 -0800

    Added router functionality

--- src/Template.js
+++ src/Template.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './Template.css';
+//Import components as well
+import loginComp from './components/login'
 
 class Template extends Component {
   render() {
     return (
-      <div className="Template">
-        <header className="Template-header">
-          <img src={logo} className="Template-logo" alt="logo" />
-          <h1 className="Template-title">Welcome to React</h1>
-        </header>
-        <p className="Template-intro">
-          To get started, edit <code>src/Template.js</code> and save to reload.
-        </p>
+      <div className="main">
+        <Switch>
+          <Route exact path='/' component={loginComp} />
+        </Switch>
       </div>
     );
   }
---
+++ src/components/login/login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class loginPage extends Component {
+
+  render() {
+    return (
+      <div>
+          <h2>Hello World!</h2>
+      </div>
+    )
+  }
+}
