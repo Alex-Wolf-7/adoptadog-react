47d31f5717b57042c22b1060bc928ab508ad8071
CubicDolphin <alexwolf737@gmail.com>
2018-03-03 21:18:07 -0800

    Fixed Header dropdown and logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,86 +4,110 @@ import logo from "./logo.jpg";
 import "./noMargins.css";
 
 const Header = () => {
-  const clearance = localStorage.getItem("clearance");
+
+  function logout() {
+    localStorage.setItem("clearance", "");
+  }
+
   var dropDownStyle = { display: "none" };
-  if (clearance == "admin") {
+  function controlMenu() {
+    if (document.getElementById("dropDown").style.display === "none") {
+      document.getElementById("dropDown").style.display = "inline-block";
+    }
+    else {
+      document.getElementById("dropDown").style.display = "none";
+    }
+  }
+
+  const clearance = localStorage.getItem("clearance");
+
+  if (clearance === "admin") {
     return (
-      <div class="header-wrapper">
+      <div className="header-wrapper">
         <img src={logo} id="logo" alt="logo" />
-        <div class="nav-bar">
-          <a class="header-link hide-link" href="kennel">
-            <button class="header-button" type="button">Kennel</button>
+        <div className="nav-bar">
+          <a className="header-link hide-link" href="kennel">
+            <button className="header-button" type="button">Kennel</button>
           </a>
-          <a class="header-link hide-link" href="admin-contact">
-            <button class="header-button" type="button">Contact Adopter</button>
+          <a className="header-link hide-link" href="admin-contact">
+            <button className="header-button" type="button">Contact Adopter</button>
           </a>
-          <a class="header-link hide-link" href="view-application">
-            <button class="header-button" type="button">View Application</button>
+          <a className="header-link hide-link" href="view-application">
+            <button className="header-button" type="button">View Application</button>
           </a>
-          <a class="header-link hide-link" href="checklist-admin">
-            <button class="header-button" type="button">Adopter Checklist</button>
+          <a className="header-link hide-link" href="checklist-admin">
+            <button className="header-button" type="button">Adopter Checklist</button>
           </a>
-          <button class="header-button" type="button" onclick="logout()">Logout</button>
-          <div class="dropdown-holder" onclick="controlMenu()">
-            <div class="dropdown-menu"></div>
-            <div class="dropdown-menu"></div>
-            <div class="dropdown-menu"></div>
+          <a className="header-link hide-link" href="/">
+            <button className="header-button" type="button" onClick={logout}>Logout</button>
+          </a>
+          <button className="header-button" type="button" onClick={logout}>Logout</button>
+          <div className="dropdown-holder" onClick={controlMenu}>
+            <div className="dropdown-menu"></div>
+            <div className="dropdown-menu"></div>
+            <div className="dropdown-menu"></div>
           </div>
           <ul id="dropDown" style={dropDownStyle}>
-            <a class="dropdown-link hide-link" href="kennel">
-              <li class="dropdown-button">Kennel</li>
+            <a className="dropdown-link hide-link" href="kennel">
+              <li className="dropdown-button">Kennel</li>
             </a>
-            <a class="dropdown-link hide-link" href="admin-contact">
-              <li class="dropdown-button">Contact Adopter</li>
+            <a className="dropdown-link hide-link" href="admin-contact">
+              <li className="dropdown-button">Contact Adopter</li>
             </a>
-            <a class="dropdown-link hide-link" href="view-application">
-              <li class="dropdown-button">View Application</li>
+            <a className="dropdown-link hide-link" href="view-application">
+              <li className="dropdown-button">View Application</li>
             </a>
-            <a class="dropdown-link hide-link" href="checklist-admin">
-              <li class="dropdown-button">Adopter Checklist</li>
+            <a className="dropdown-link hide-link" href="checklist-admin">
+              <li className="dropdown-button">Adopter Checklist</li>
             </a>
-            <li class="dropdown-link hide-link" onclick="logout()" class="dropdown-button">Logout</li>
+            <a className="dropdown-link hide-link" href="/">
+              <li className="dropdown-button" onClick={logout}>Logout</li>
+            </a>
           </ul>
         </div>
       </div>
     );
   } else {
     return (
-      <div class="header-wrapper">
+      <div className="header-wrapper">
         <img src={logo} id="logo" alt="logo" />
-        <div class="nav-bar">
-          <a class="header-link hide-link" href="home">
-            <button class="header-button" type="button">Home</button>
+        <div className="nav-bar">
+          <a className="header-link hide-link" href="home">
+            <button className="header-button" type="button">Home</button>
           </a>
-          <a class="header-link hide-link" href="about-us">
-            <button class="header-button" type="button">About Us</button>
+          <a className="header-link hide-link" href="about-us">
+            <button className="header-button" type="button">About Us</button>
           </a>
-          <a class="header-link hide-link" href="kennel">
-            <button class="header-button" type="button">Kennel</button>
+          <a className="header-link hide-link" href="kennel">
+            <button className="header-button" type="button">Kennel</button>
           </a>
-          <a class="header-link hide-link" href="checklist">
-            <button class="header-button" type="button">Checklist</button>
+          <a className="header-link hide-link" href="checklist">
+            <button className="header-button" type="button">Checklist</button>
           </a>
-          <button class="header-button" type="button" onclick="logout()">Logout</button>
-          <div class="dropdown-holder" onclick="controlMenu()">
-            <div class="dropdown-menu"></div>
-            <div class="dropdown-menu"></div>
-            <div class="dropdown-menu"></div>
+          <a className="header-link hide-link" href="/">
+            <button className="header-button" type="button" onClick={logout}>Logout</button>
+          </a>
+          <div className="dropdown-holder" onClick={controlMenu}>
+            <div className="dropdown-menu"></div>
+            <div className="dropdown-menu"></div>
+            <div className="dropdown-menu"></div>
           </div>
           <ul id="dropDown" style={dropDownStyle}>
-            <a class="dropdown-link hide-link" href="home">
-              <li class="dropdown-button">Home</li>
+            <a className="dropdown-link hide-link" href="home">
+              <li className="dropdown-button">Home</li>
             </a>
-            <a class="dropdown-link hide-link" href="about-us">
-              <li class="dropdown-button">About Us</li>
+            <a className="dropdown-link hide-link" href="about-us">
+              <li className="dropdown-button">About Us</li>
             </a>
-            <a class="dropdown-link hide-link" href="kennel">
-              <li class="dropdown-button">Kennel</li>
+            <a className="dropdown-link hide-link" href="kennel">
+              <li className="dropdown-button">Kennel</li>
             </a>
-            <a class="dropdown-link hide-link" href="checklist">
-              <li class="dropdown-button">Checklist</li>
+            <a className="dropdown-link hide-link" href="checklist">
+              <li className="dropdown-button">Checklist</li>
             </a>
-            <li class="dropdown-link hide-link" onclick="logout()" class="dropdown-button">Logout</li>
+            <a className="dropdown-link hide-link" href="/">
+              <li className="dropdown-button" onClick={logout}>Logout</li>
+            </a>
           </ul>
         </div>
       </div>
