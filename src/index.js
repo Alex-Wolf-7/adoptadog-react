751140bf1c4a6a5eb92160937d645d8d3e3d41dc
Ryan Willett <rwillett@ucsd.edu>
2018-03-15 14:27:51 -0700

    I'm not sure if it works but it compiles

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './Router.js';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
 const store = configureStore();
 
 ReactDOM.render((
-  <Provider store={store}>
-    <App />
+  <Provider store={store.store}>
+    <PersistGate loading={null} persistor={store.persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 
   ), document.getElementById('root'));
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,30 @@
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers/rootReducer';
-import thunk from 'redux-thunk';
+// import {createStore, applyMiddleware} from 'redux';
+// import rootReducer from '../reducers/rootReducer';
+// import thunk from 'redux-thunk';
+//
+// export default function configureStore() {
+//   return createStore(
+//     rootReducer,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//     applyMiddleware(thunk)
+//   );
+// }
+
+import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
+
+import rootReducer from '../reducers/rootReducer'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export default function configureStore() {
-  return createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
-  );
+  let store = createStore(persistedReducer)
+  let persistor = persistStore(store)
+  return { store, persistor }
 }
