130727621b15348d10defbbdbad35602b5878996
Ryan Willett <rwillett@ucsd.edu>
2018-03-17 12:13:43 -0700

    Login Reducer tests done

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -3,7 +3,6 @@ import {SET_CLEARANCE} from '../actions/actionTypes';
 
 export default function setClearance(state = initialState.clearance, action) {
   let newState;
-  console.log("STATE IS: " + initialState.clearance);
   switch (action.type) {
 
     case SET_CLEARANCE:
